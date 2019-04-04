1bf2ace84bb573dab446480a4237e64e60b77138
Gino Desangles <blushingbox@yahoo.com>
2019-04-04 17:15:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,8 @@ class Transfer
   
   def execute_transaction
     if sender.balance < amount
-      "Transaction rejected. Please check your account balance."
+      @status = 'rejected'
+      return "Transaction rejected. Please check your account balance."
     else
       sender.balance -= amount
       receiver.balance += amount
